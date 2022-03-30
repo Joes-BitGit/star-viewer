c7e74fe1bfb44c956aec4b548d3d45f083410a70
Joes-BitGit <josephnalmeida@gmail.com>
2022-03-30 16:33:57 -0700

    async/await syntax for axios functions

--- src/App.js
+++ src/App.js
@@ -14,48 +14,99 @@ function App() {
   useEffect(() => {
     setLoading(true);
     // axios serves in a similar way to the fetch api that is native to the browser
-    axios
-      .get(`${Config.apiEndpoint}/api/v2/bodies`, {
-        headers: {
-          "X-Requested-With": "XMLHttpRequest",
-          Authorization: `Basic ${btoa(`${Config.appId}:${Config.appSecret}`)}`,
-        },
-      })
-      .then((response) => {
+    async function getBodies() {
+      try {
+        const response = await axios.get(
+          `${Config.apiEndpoint}/api/v2/bodies`,
+          {
+            headers: {
+              "X-Requested-With": "XMLHttpRequest",
+              Authorization: `Basic ${btoa(
+                `${Config.appId}:${Config.appSecret}`
+              )}`,
+            },
+          }
+        );
         setData(response.data.data);
         setLoading(false);
-      });
-
-    axios
-      .post(
-        `${Config.apiEndpoint}/api/v2/studio/star-chart`,
-        {
-          style: "default",
-          observer: {
-            latitude: 33.775867,
-            longitude: -84.39733,
-            date: "2019-12-20",
-          },
-          view: {
-            type: "constellation",
-            parameters: {
-              constellation: "vul", // 3 letter constellation id
+      } catch (error) {}
+    }
+    async function getStarChart() {
+      try {
+        const response = await axios.post(
+          `${Config.apiEndpoint}/api/v2/studio/star-chart`,
+          {
+            style: "default",
+            observer: {
+              latitude: 33.775867,
+              longitude: -84.39733,
+              date: "2019-12-20",
+            },
+            view: {
+              type: "constellation",
+              parameters: {
+                constellation: "vul", // 3 letter constellation id
+              },
             },
           },
-        },
-        {
-          headers: {
-            "X-Requested-With": "XMLHttpRequest",
-            Authorization: `Basic ${btoa(
-              `${Config.appId}:${Config.appSecret}`
-            )}`,
-          },
-        }
-      )
-      .then((response) => {
+          {
+            headers: {
+              "X-Requested-With": "XMLHttpRequest",
+              Authorization: `Basic ${btoa(
+                `${Config.appId}:${Config.appSecret}`
+              )}`,
+            },
+          }
+        );
+
         setImageUrl(response.data.data.imageUrl);
         setLoading(false);
-      });
+      } catch (error) {}
+    }
+    getBodies();
+    getStarChart();
+    // axios
+    //   .get(`${Config.apiEndpoint}/api/v2/bodies`, {
+    //     headers: {
+    //       "X-Requested-With": "XMLHttpRequest",
+    //       Authorization: `Basic ${btoa(`${Config.appId}:${Config.appSecret}`)}`,
+    //     },
+    //   })
+    //   .then((response) => {
+    //     setData(response.data.data);
+    //     setLoading(false);
+    //   });
+
+    // axios
+    //   .post(
+    //     `${Config.apiEndpoint}/api/v2/studio/star-chart`,
+    //     {
+    //       style: "default",
+    //       observer: {
+    //         latitude: 33.775867,
+    //         longitude: -84.39733,
+    //         date: "2019-12-20",
+    //       },
+    //       view: {
+    //         type: "constellation",
+    //         parameters: {
+    //           constellation: "vul", // 3 letter constellation id
+    //         },
+    //       },
+    //     },
+    //     {
+    //       headers: {
+    //         "X-Requested-With": "XMLHttpRequest",
+    //         Authorization: `Basic ${btoa(
+    //           `${Config.appId}:${Config.appSecret}`
+    //         )}`,
+    //       },
+    //     }
+    //   )
+    //   .then((response) => {
+    //     setImageUrl(response.data.data.imageUrl);
+    //     setLoading(false);
+    //   });
   }, []);
 
   return (
