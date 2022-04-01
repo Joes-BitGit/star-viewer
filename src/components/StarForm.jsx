5398c415181baece37e96129338ca4e5359c1669
Joes-BitGit <josephnalmeida@gmail.com>
2022-03-31 18:10:47 -0700

    handles submission of constellations then renders

--- src/components/StarForm.jsx
+++ src/components/StarForm.jsx
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Config from "../app/config.json";
 
 export const StarForm = () => {
-  const [value, setValue] = useState("and");
+  const [query, setQuery] = useState("and");
+  const [imageUrl, setImageUrl] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [submitted, setSubmitted] = useState(query);
+
   // best way to store a static object?
   const constellations = {
     and: "Andromeda",
@@ -94,33 +100,85 @@ export const StarForm = () => {
     vol: "Volans",
     Vul: "Vulpecula",
   };
+
+  useEffect(() => {
+    async function getStarChart() {
+      setLoading(true);
+      let isMounted = true;
+      try {
+        const response = await axios.post(
+          `${Config.apiEndpoint}/api/v2/studio/star-chart`,
+          // should be able to make this entire header editable to the user
+          {
+            style: "default",
+            observer: {
+              latitude: 33.775867,
+              longitude: -84.39733,
+              date: "2019-12-20",
+            },
+            view: {
+              type: "constellation",
+              parameters: {
+                constellation: submitted, // 3 letter constellation id
+              },
+            },
+          },
+          {
+            headers: {
+              "X-Requested-With": "XMLHttpRequest",
+              Authorization: `Basic ${btoa(
+                `${Config.appId}:${Config.appSecret}`
+              )}`,
+            },
+          }
+        );
+        if (isMounted) setImageUrl(response.data.data.imageUrl);
+
+        setLoading(false);
+      } catch (error) {
+        console.log("post error in starform");
+      }
+      return () => {
+        isMounted = false;
+      };
+    }
+    getStarChart();
+  }, [submitted]);
+
   const handleChange = (event) => {
-    setValue(event.target.value);
+    setQuery(event.target.value);
     console.log(event.target.value);
   };
 
   const handleSubmit = (event) => {
-    alert("You have chosen: " + constellations[value]);
+    setSubmitted(query);
     event.preventDefault();
+
+    alert("You have chosen: " + constellations[query]);
   };
 
-  return (
-    <form onSubmit={(e) => handleSubmit(e)}>
-      <label>
-        Pick your Constellation:
-        <select value={value} onChange={(e) => handleChange(e)}>
-          <option value={value} aria-disabled disabled>
-            {constellations[value]}
-          </option>
-          {/* You don't need to check hasOwnProperty when iterating on keys if you're using a simple object or one you made yourself with {}. */}
-          {Object.keys(constellations).map((item) => (
-            <option value={item} key={item}>
-              {constellations[item]}
-            </option>
-          ))}
-        </select>
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+  return loading ? (
+    <div>Lading...</div>
+  ) : (
+    <>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label>
+          Pick your Constellation:
+          <select value={query} onChange={(e) => handleChange(e)}>
+            {/* <option value={query} aria-disabled disabled>
+              {constellations[query]}
+            </option> */}
+            {/* You don't need to check hasOwnProperty when iterating on keys if you're using a simple object or one you made yourself with {}. */}
+            {Object.keys(constellations).map((item) => (
+              <option value={item} key={item}>
+                {constellations[item]}
+              </option>
+            ))}
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <img src={imageUrl} alt={`constellation ${constellations[query]}`} />
+    </>
   );
 };
