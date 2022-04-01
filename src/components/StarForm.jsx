e712aa386e03c35fc99c8b9e6ac73141a50206ef
Joes-BitGit <josephnalmeida@gmail.com>
2022-04-01 13:34:16 -0700

    style handler, extracted the useeffect

--- src/components/StarForm.jsx
+++ src/components/StarForm.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import Config from "../app/config.json";
+import { useStarChartApi } from "./useStarChartApi.jsx";
 
 export const StarForm = () => {
   const [query, setQuery] = useState("and");
-  const [imageUrl, setImageUrl] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [submitted, setSubmitted] = useState(query);
+
+  const [starStyle, setStarStyle] = useState("default");
+  const [{ imageUrl, loading }, doSubmit, doStyle] = useStarChartApi();
 
   // best way to store a static object?
   const constellations = {
@@ -100,61 +100,23 @@ export const StarForm = () => {
     vol: "Volans",
     Vul: "Vulpecula",
   };
-
-  useEffect(() => {
-    async function getStarChart() {
-      setLoading(true);
-      let isMounted = true;
-      try {
-        const response = await axios.post(
-          `${Config.apiEndpoint}/api/v2/studio/star-chart`,
-          // should be able to make this entire header editable to the user
-          {
-            style: "default",
-            observer: {
-              latitude: 33.775867,
-              longitude: -84.39733,
-              date: "2019-12-20",
-            },
-            view: {
-              type: "constellation",
-              parameters: {
-                constellation: submitted, // 3 letter constellation id
-              },
-            },
-          },
-          {
-            headers: {
-              "X-Requested-With": "XMLHttpRequest",
-              Authorization: `Basic ${btoa(
-                `${Config.appId}:${Config.appSecret}`
-              )}`,
-            },
-          }
-        );
-        if (isMounted) setImageUrl(response.data.data.imageUrl);
-
-        setLoading(false);
-      } catch (error) {
-        console.log("post error in starform");
-      }
-      return () => {
-        isMounted = false;
-      };
-    }
-    getStarChart();
-  }, [submitted]);
+  const StarStyles = ["default", "inverted", "navy", "red"];
 
   const handleChange = (event) => {
     setQuery(event.target.value);
-    console.log(event.target.value);
+    console.log("constellation id: ", event.target.value);
   };
 
   const handleSubmit = (event) => {
-    setSubmitted(query);
+    doSubmit(query);
     event.preventDefault();
 
     alert("You have chosen: " + constellations[query]);
+  };
+
+  const handleStyleChange = (event) => {
+    doStyle(event.target.value);
+    console.log("style: ", event.target.value);
   };
 
   return loading ? (
@@ -165,9 +127,6 @@ export const StarForm = () => {
         <label>
           Pick your Constellation:
           <select value={query} onChange={(e) => handleChange(e)}>
-            {/* <option value={query} aria-disabled disabled>
-              {constellations[query]}
-            </option> */}
             {/* You don't need to check hasOwnProperty when iterating on keys if you're using a simple object or one you made yourself with {}. */}
             {Object.keys(constellations).map((item) => (
               <option value={item} key={item}>
@@ -176,6 +135,15 @@ export const StarForm = () => {
             ))}
           </select>
         </label>
+        <label>
+          Pick Your Style:
+          <select value={starStyle} onChange={(e) => handleStyleChange(e)}>
+            {StarStyles.map((styletype) => (
+              <option key={styletype}>{styletype}</option>
+            ))}
+          </select>
+        </label>
+
         <input type="submit" value="Submit" />
       </form>
       <img src={imageUrl} alt={`constellation ${constellations[query]}`} />
