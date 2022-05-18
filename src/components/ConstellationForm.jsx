7bfa5b6de077a09224a9dab651a3761969309058
Joes-BitGit <josephnalmeida@gmail.com>
2022-05-18 13:53:18 -0700

    styled form w/ reactbootstrap

--- src/components/ConstellationForm.jsx
+++ src/components/ConstellationForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container, Form, Row, Col } from "react-bootstrap";
 import { constellations, StarStyles } from "../data/Constellations";
 
 export const ConstellationForm = ({
@@ -19,25 +20,35 @@ export const ConstellationForm = ({
 
   return (
     <>
-      <label>
-        Pick your Constellation:
-        <select value={query} onChange={(e) => handleChange(e)}>
-          {/* You don't need to check hasOwnProperty when iterating on keys if you're using a simple object or one you made yourself with {}. */}
-          {Object.keys(constellations).map((item) => (
-            <option value={item} key={item}>
-              {constellations[item]}
-            </option>
-          ))}
-        </select>
-      </label>
-      <label>
-        Pick Your Style:
-        <select value={starStyle} onChange={(e) => handleStyleChange(e)}>
-          {StarStyles.map((styletype) => (
-            <option key={styletype}>{styletype}</option>
-          ))}
-        </select>
-      </label>
+      <Container>
+        <Row>
+          <Form.Group as={Col} className="mb-3" controlId="formConstellation">
+            <Form.Label>Pick Your Constellation:</Form.Label>
+            <Form.Select value={query} onChange={(e) => handleChange(e)}>
+              {/* You don't need to check hasOwnProperty when iterating on keys if you're using a simple object or one you made yourself with {}. */}
+              {Object.keys(constellations).map((item) => (
+                <option value={item} key={item}>
+                  {constellations[item]}
+                </option>
+              ))}
+            </Form.Select>
+            <Form.Text className="text-muted">
+              Pick One You've Never Seen!
+            </Form.Text>
+          </Form.Group>
+          <Form.Group as={Col} className="mb-3" controlId="formStyle">
+            <Form.Label>Pick Your Style:</Form.Label>
+            <Form.Select
+              value={starStyle}
+              onChange={(e) => handleStyleChange(e)}
+            >
+              {StarStyles.map((styletype) => (
+                <option key={styletype}>{styletype}</option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+        </Row>
+      </Container>
     </>
   );
 };
