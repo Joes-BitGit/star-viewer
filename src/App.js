d1512edd0fe4fce7411d94940bd99b827fe23a85
Joes-BitGit <josephnalmeida@gmail.com>
2022-05-21 14:52:23 -0700

    added margin to navbar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Container from "react-bootstrap/Container";
 function App() {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="dark" variant="dark" className="mb-3">
         <Container fluid>
           <Navbar.Brand href="#home">Star Patterns</Navbar.Brand>
         </Container>
