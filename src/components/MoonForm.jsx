0d81cd7c720c4d6c501d28430f705ebf80377f81
Joes-BitGit <josephnalmeida@gmail.com>
2022-05-25 14:01:29 -0700

    add type and orientation options

--- src/components/MoonForm.jsx
+++ src/components/MoonForm.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Col, Form, Row } from "react-bootstrap";
-import { backgroundStyle, moonFormat, moonStyle } from "../data/Constellations";
+import {
+  backgroundStyle,
+  moonFormat,
+  moonOrientation,
+  moonStyle,
+  moonType,
+} from "../data/Constellations";
 
 export const MoonForm = ({
   format,
@@ -28,21 +34,27 @@ export const MoonForm = ({
     setBackground(event.target.value);
   };
 
+  const handleTypeChange = (event) => {
+    setType(event.target.value);
+  };
+
+  const handleMoonOrientation = (event) => {
+    setOrientation(event.target.value);
+  };
+
   return (
     <>
       <Row className="mb-3">
         <Form.Group as={Col} controlId="formMoon">
           <Form.Label>Pick Your Moon:</Form.Label>
-          {/* state needs to be updated */}
           <Form.Select value={format} onChange={(e) => handleFormatChange(e)}>
             {moonFormat.map((moonFormat) => (
-              <option key={moonFormat}>format</option>
+              <option key={moonFormat}>{moonFormat}</option>
             ))}
           </Form.Select>
         </Form.Group>
         <Form.Group as={Col} controlId="formMoonStyle">
           <Form.Label>Pick Your Moon Style:</Form.Label>
-          {/* state must be updated */}
           <Form.Select value={style} onChange={(e) => handleMoonChange(e)}>
             {moonStyle.map((moonStyle) => (
               <option key={moonStyle}>{moonStyle}</option>
@@ -51,13 +63,34 @@ export const MoonForm = ({
         </Form.Group>
         <Form.Group as={Col} controlId="formBackgroundStyle">
           <Form.Label>Pick Your Background Style:</Form.Label>
-          {/* state must be updated */}
           <Form.Select
             value={background}
             onChange={(e) => handleBackgroundChange(e)}
           >
             {backgroundStyle.map((backgroundStyle) => (
               <option key={backgroundStyle}>{backgroundStyle}</option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+      </Row>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formMoonType">
+          <Form.Label>Pick Your Moon Type:</Form.Label>
+          <Form.Select value={type} onChange={(e) => handleTypeChange(e)}>
+            {moonType.map((moonType) => (
+              <option key={moonType}>{moonType}</option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+        <Form.Group as={Col} controlId="formMoonStyle">
+          <Form.Label>Pick Your Moon Style:</Form.Label>
+          {/* state must be updated */}
+          <Form.Select
+            value={orientation}
+            onChange={(e) => handleMoonOrientation(e)}
+          >
+            {moonOrientation.map((moonOrientation) => (
+              <option key={moonOrientation}>{moonOrientation}</option>
             ))}
           </Form.Select>
         </Form.Group>
